68e6daebbf0c3f90f366f2b1a069c75d2cf6f8ae
cipdev <cipopescu@proton.me>
2025-01-06 14:40:37 +0200

    Add discord invite link

--- src/pages/discord/discord.component.js
+++ src/pages/discord/discord.component.js
@@ -2,14 +2,38 @@ import { LitElement, css, html } from "lit";
 import mixinStyles from "../styles.js";
 
 export class AoeDiscord extends LitElement {
-  static properties = {};
+  static properties = {
+    instant_invite: { String },
+  };
 
   constructor() {
     super();
   }
 
+  async firstUpdated() {
+    this.instant_invite = await fetch(
+      "https://discord.com/api/guilds/1197960195532410962/widget.json"
+    )
+      .then((r) => r.json())
+      .then(({ instant_invite }) => instant_invite);
+  }
+
   render() {
     return html`<div class="page-container">
+      ${this.instant_invite
+        ? html`<form action="${this.instant_invite}">
+            <label>Invite link:</label>
+            <input
+              type="text"
+              id="instant_invite"
+              name="instant_invite"
+              readonly
+              size="${this.instant_invite.length}"
+              value="${this.instant_invite}"
+            />
+            <input type="submit" value="Join AoE!" />
+          </form>`
+        : ""}
       <iframe
         src="https://discord.com/widget?id=1197960195532410962&theme=dark"
         allowtransparency="true"
@@ -22,6 +46,36 @@ export class AoeDiscord extends LitElement {
   static styles = [
     mixinStyles,
     css`
+      form {
+        display: block;
+        margin: 1rem auto;
+        text-align: center;
+      }
+      form > * {
+        line-height: 2rem;
+        font-size: 1rem;
+        border-radius: 8px;
+        padding: 0 1rem;
+      }
+      [type="submit"] {
+        font-weight: bolder;
+        position: relative;
+        color: inherit;
+        background-color: var(--acc2);
+        border: 1px solid var(--border);
+        margin: 0 2rem;
+      }
+      [type="submit"]:hover {
+        scale: 1.1;
+        color: var(--acc3);
+        cursor: pointer;
+      }
+      [type="text"] {
+        border: 1px solid var(--border);
+      }
+      label {
+        font-weight: bolder;
+      }
       iframe {
         display: block;
         margin: 1rem auto;
