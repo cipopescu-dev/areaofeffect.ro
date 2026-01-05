d04a5f379c243fecef7ae8414d43ff4851e34bfe
Cip <cipopescu@proton.me>
2026-01-05 16:24:40 +0200

    2.0.0 release

---
+++ assets/NotFound.component-ae_xoXYN.js
@@ -0,0 +1,1 @@
+import{i as e,x as t,a as o}from"./index-lcOe9ydS.js";class s extends e{static properties={};render(){return t`<h2>404 The requested page could not be found</h2>`}static styles=[o``]}window.customElements.define("aoe-notfound",s);export{s as NotFound};
