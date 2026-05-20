146b23312ea0d80fbef7ef446ca49458166ba395
Cipopescu <cipopescu@proton.me>
2026-05-21 01:24:08 +0300

    public calendar

--- assets/NotFound.component-C91pPcCy.js
+++
@@ -1,1 +0,0 @@
-import{i as e,b as t,a as o}from"./index-DrajzW_m.js";class s extends e{static properties={};render(){return t`<h2>404 The requested page could not be found</h2>`}static styles=[o``]}window.customElements.define("aoe-notfound",s);export{s as NotFound};
